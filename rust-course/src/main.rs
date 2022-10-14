c063d5913bdd615bdca1e4d17330c5b017e94fb0
shoaibshebi <shoaib4030891@gmail.com>
2022-10-14 15:03:40 +0500

    variables, & shawoing

--- rust-course/src/main.rs
+++ rust-course/src/main.rs
@@ -2,5 +2,15 @@
 //cargo-watch -qc -x run -x clippy
 
 fn main() {
-    println!("Hello, world!");
+    let first_name = "John";
+    let first_name = first_name;
+    println!("hellow {}", first_name);
+
+    let personal_data = (32, "shoaib");
+    let (age, name) = personal_data;
+    println!(" {} {}", age, name);
+
+    let firstint = 32;
+    let secint = firstint;
+    println!("{} {}", firstint, secint);
 }
