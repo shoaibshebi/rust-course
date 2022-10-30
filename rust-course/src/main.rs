c643493881370eee320c7c13af08c336a2ae1de6
shoaibshebi <shoaib4030891@gmail.com>
2022-10-30 08:09:20 +0500

    clear - option and error, result

--- rust-course/src/main.rs
+++ rust-course/src/main.rs
@@ -9,6 +9,10 @@ fn get_values() -> (String, String, i32) {
     ("Hellow".to_string(), "World".to_string(), 30)
 }
 
+fn get_user_name() -> Result<String, ()> {
+    Ok("Shoaib ".to_string())
+}
+
 fn main() {
     let (hello, _, _) = get_values();
 
@@ -44,4 +48,21 @@ fn main() {
     for value in values.iter() {
         println!("{}", value)
     }
+
+    let name = None;
+    let unwrapped = name.unwrap_or("Shaoib");
+    println!("name is {} ", unwrapped);
+
+    // Result<T, E> is the type used for returning and propagating errors. It is an enum with the variants, Ok(T),
+    // representing success and containing a value, and Err(E), representing error and containing an error value.
+    let valueErr: Result<&str, Box<dyn std::error::Error>> = Ok("Hello");
+    match valueErr {
+        Ok(value) => println!("value is {} ", value),
+        Err(err) => println!("Err is {} ", err),
+    }
+
+    get_user_name().expect("hellow there");
+
+    let is_ok = get_user_name().is_ok();
+    let is_err = get_user_name().is_err();
 }
